440ae7d15d72a6e8fd815de88dfc388cd3248a86
cliffyschool <cliffyschool@gmail.com>
2016-01-06 21:10:20 -0600

    WIP - got level 1 rotate working

--- rotateMatrix/src/test/scala/Solution.scala
+++ rotateMatrix/src/test/scala/Solution.scala
@@ -37,35 +37,70 @@ class Solution {
         .map(row => (0 to colCount - 1)
         .map(col => Cell(row,col, matrix(row)(col))))
         .flatten
-        //.filter(cell => cell.row == 0 || cell.row == rowCount - 1 || cell.col == 0 || cell.col == colCount -1)
+        .filter(cell => cell.row == 0 || cell.row == rowCount - 1 || cell.col == 0 || cell.col == colCount -1)
       val rotated = cells
         .map(cell => nextPos(cell, cells.head, cells.last))
+        .sortBy(c => (c.row,c.col))
 
-        println(rotated.sortBy(c => (c.row,c.col)))
       rotated
     }
 
     def nextPos(cell: Cell, topLeft: Cell, bottomRight: Cell) = {
      cell match {
-       case topLeft => Cell(topLeft.row + 1, topLeft.col, topLeft.num)
-       case bottomRight => Cell(bottomRight.row -1, bottomRight.col, bottomRight.num)
+       case c @ Cell(topLeft.col, topLeft.row, topLeft.num) =>
+         Cell(topLeft.row + 1, topLeft.col, topLeft.num)
+       case c @ Cell(bottomRight.row, bottomRight.col, bottomRight.num) =>
+         Cell(bottomRight.row -1, bottomRight.col, bottomRight.num)
        //topRight
-       case c @ Cell(topLeft.row, bottomRight.col, _) => Cell(topLeft.row, bottomRight.col - 1, c.num)
+       case c @ Cell(topLeft.row, bottomRight.col, _) =>
+         Cell(topLeft.row, bottomRight.col - 1, c.num)
        // bottomLeft
-       case c @ Cell(bottomRight.row, topLeft.col, _) => Cell(bottomRight.row, topLeft.col + 1, c.num)
+       case c @ Cell(bottomRight.row, topLeft.col, _) =>
+         Cell(bottomRight.row, topLeft.col + 1, c.num)
        // top
-       case c @ Cell(topLeft.row,_, _) => Cell(c.row, c.col - 1, c.num)
+       case c @ Cell(topLeft.row,_, _) =>
+         Cell(c.row, c.col - 1, c.num)
        // left
-       case c @ Cell(_, topLeft.col, _) => Cell(c.row + 1, c.col, c.num)
+       case c @ Cell(_, topLeft.col, _) =>
+         Cell(c.row + 1, c.col, c.num)
        // bottom
-       case c @ Cell(bottomRight.row, _, _) => Cell(c.row, c.col + 1, c.num)
+       case c @ Cell(bottomRight.row, _, _) =>
+         Cell(c.row, c.col + 1, c.num)
        // right
-       case c @ Cell(_, bottomRight.col, _) => Cell(c.row - 1, c.col, c.num)
+       case c @ Cell(_, bottomRight.col, _) =>
+         Cell(c.row - 1, c.col, c.num)
      }
     }
 }
 
 class SolutionSpec extends Specification {
+    "Given topLeft, nextPos" should {
+      val sol = new Solution();
+      val topLeft = Cell(0,0,1)
+      val bottomRight = Cell(1,1,2)
+      val rotated = sol.nextPos(topLeft, topLeft, bottomRight)
+      "rotate it once" in {
+        rotated must equalTo(Cell(1,0,1))
+        
+      }
+    }
+
+    "Given a small matrix, rotateMatrix" should {
+      val sol = new Solution();
+      val matrix = Array.ofDim[Int](2,2)
+      matrix(0) = Array(1,2)
+      matrix(1) = Array(3,4)
+     
+      val rotated = sol.rotateMatrix(matrix)
+      println(rotated)
+      "rotate it" in {
+        rotated(0) must equalTo(Cell(0,0,2))
+        rotated(1) must equalTo(Cell(0,1,4))
+        rotated(2) must equalTo(Cell(1,0,1))
+        rotated(3) must equalTo(Cell(1,1,3))
+      }
+    }
+    
     "Given a matrix, rotateMatrix" should {
         val s = new Solution()
         val matrix = Array.ofDim[Int](5,4)
@@ -83,7 +118,7 @@ class SolutionSpec extends Specification {
         expected(4) = Array(4,3,2,1)
         
         val result = s.rotateMatrix(matrix)
-
+println(result)
         "rotate it" in {
             result(0) must equalTo(expected(0))
             result(1) must equalTo(expected(1))
