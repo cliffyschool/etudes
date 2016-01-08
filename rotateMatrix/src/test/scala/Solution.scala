3b81dc7956436b842d6b2411a4c3941738d353a3
cliffyschool <cliffyschool@gmail.com>
2016-01-07 21:12:24 -0600

    rotateMatrix

--- rotateMatrix/src/test/scala/Solution.scala
+++ rotateMatrix/src/test/scala/Solution.scala
@@ -27,30 +27,27 @@ object Solution{
 case class Cell(row: Int, col: Int, num: Int)
 
 class Solution {
-    def rotateMatrix(matrix: Array[Array[Int]]) = {
+
+    def rotateMatrix(matrix: Array[Array[Int]], times: Int) = {
       val maxRow = matrix.length - 1
       val maxCol = matrix(0).length - 1
       val levelCount = Math.min(matrix.length, matrix(0).length) / 2
-      val rotated = (0 to levelCount - 1)
-        .map(l => (Cell(l,l,matrix(l)(l)),
-                    Cell(maxRow-l, maxCol-l,matrix(maxRow-l)(maxCol-l))))
-      .map(cornerTuple => (cornerTuple._1, cornerTuple._2, cellsInLevel(cornerTuple._1, cornerTuple._2, matrix)))
-      .flatMap(levelCells => levelCells._3.map(c => rotateCell(c, levelCells._1, levelCells._2, 1)))
+      val corners = (0 until levelCount)
+        .map(l => l -> (Cell(l,l, matrix(l)(l)), Cell(maxRow-l, maxCol-l, matrix(maxRow-l)(maxCol-l)))).toMap
+      println(corners)
+      val rotated = (0 to maxRow)
+        .flatMap(row => (0 to maxCol)
+          .map(col => Cell(row,col,matrix(row)(col))))
+        .map(cell => {
+          val cellCorners = corners(cellLevel(cell, 0, maxRow, 0, maxCol))
+          rotateCell(cell, cellCorners._1, cellCorners._2, times)})
+            .sortBy(cell => (cell.row,cell.col))
 
       rotated
     }
 
-    def cellsInLevel(topLeft: Cell, bottomRight: Cell, matrix:Array[Array[Int]]) = {
-      val cells =
-      // topLeft -> bottomLeft
-      (topLeft.row to bottomRight.row).map(row => Cell(row, topLeft.col, matrix(row)(topLeft.col))) ++
-      // bottomLeft -> bottomRight
-      (topLeft.col + 1 to bottomRight.col).map(col => Cell(bottomRight.row, col, matrix(bottomRight.row)(col))) ++
-      // bottomRight -> topRight
-      (bottomRight.row - 1 to topLeft.row by -1).map(row => Cell(row, bottomRight.col, matrix(row)(bottomRight.col))) ++
-      // topRight -> topLeft
-      (bottomRight.col -1 to topLeft.col + 1 by -1).map(col => Cell(topLeft.row, col, matrix(topLeft.row)(col)))
-      cells
+    def cellLevel(cell: Cell, minRow: Int, maxRow: Int, minCol: Int, maxCol: Int) = {
+      Seq(cell.row-minRow, maxRow-cell.row, cell.col-minCol, maxCol-cell.col).min
     }
 
     def rotateCell(cell: Cell, topLeft:Cell, bottomRight: Cell, times: Int) = {
@@ -98,14 +95,14 @@ class SolutionSpec extends Specification {
         
       }
     }
-
+/*
     "Given a small matrix, rotateMatrix" should {
       val sol = new Solution();
       val matrix = Array.ofDim[Int](2,2)
       matrix(0) = Array(1,2)
       matrix(1) = Array(3,4)
      
-      val rotated = sol.rotateMatrix(matrix)
+      val rotated = sol.rotateMatrix(matrix,1)
       println(rotated)
       "rotate it" in {
         rotated must contain(
@@ -115,6 +112,7 @@ class SolutionSpec extends Specification {
           Cell(1,1,3))
       }
     }
+    */
     
     "Given a matrix, rotateMatrix" should {
         val s = new Solution()
@@ -125,21 +123,26 @@ class SolutionSpec extends Specification {
         matrix(3) = Array(19,20,21,22)
         matrix(4) = Array(25,26,27,28)
 
-        val expected = Array.ofDim[Int](5,4)
+      val expected = Array.ofDim[Int](5,4)
+        matrix(0) = Array(2,3,4,10)
+        matrix(1) = Array(1,9,15,16)
+        matrix(2) = Array(7,8,21,22)
+        matrix(3) = Array(13,14,20,28)
+        matrix(4) = Array(19, 25,26,27)
+
+        val expectedAfter7 = Array.ofDim[Int](5,4)
         expected(0) = Array(28,27,26,25)
         expected(1) = Array(22,9,15,19)
         expected(2) = Array(16,8,21,13)
         expected(3) = Array(10,14,20,7)
         expected(4) = Array(4,3,2,1)
         
-        val result = s.rotateMatrix(matrix)
+        val result = s.rotateMatrix(matrix, 1)
 println(result)
         "rotate it" in {
-            result(0) must equalTo(expected(0))
-            result(1) must equalTo(expected(1))
-            result(2) must equalTo(expected(2))
-            result(3) must equalTo(expected(3))
-            result(4) must equalTo(expected(4))
+            result must contain(
+              Cell(0,0,2),Cell(1,1,9),Cell(2,2,21),Cell(3,3,28)
+            )
          }
     }
 }
