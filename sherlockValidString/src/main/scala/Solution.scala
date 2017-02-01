6228d59073e31dce2668475efe4327fe63d78fa3
cliffyschool <cliffyschool@gmail.com>
2017-01-31 19:54:03 -0600

    setup

---
+++ sherlockValidString/src/main/scala/Solution.scala
@@ -0,0 +1,23 @@
+import java.util.Scanner
+
+class Solution {
+}
+
+object Solution{
+
+  def main(args: Array[String]) : Unit = {
+    val in = new Scanner(System.in)
+    val solution = new Solution()
+
+    val s = readLine(in)
+
+    val toPrint = ""
+
+    System.out.println(toPrint)
+  }
+
+  def readLine(in: Scanner) = {
+    in.nextLine()
+  }
+
+}
