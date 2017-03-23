4095fa63ca31d7629458bc6ab70f5c867d92c1dc
cliffyschool <cliffyschool@gmail.com>
2017-03-22 20:10:32 -0500

    WIP - javafying

--- fraudulent_activity/src/main/scala/Solution.scala
+++ fraudulent_activity/src/main/scala/Solution.scala
@@ -1,49 +1,60 @@
+import java.util
 import java.util.Scanner
 
-import scala.collection.mutable
+import scala.collection.{SortedSet, mutable}
 
 class Solution {
+  case class Expenditure(amt: Int, originalPosition: Int)
+  trait HasExpenditure {
+    def exp : Expenditure
+  }
+  case class ExpenditureByAmount(exp: Expenditure) extends Comparable[ExpenditureByAmount] with HasExpenditure {
+    override def compareTo(that: ExpenditureByAmount): Int = {
+      val round1 = exp.amt - that.exp.amt
+      if (round1 == 0)
+        exp.originalPosition - that.exp.originalPosition
+      else
+        round1
+    }
+  }
+  case class ExpenditureByOriginalPos(exp: Expenditure) extends Comparable[ExpenditureByOriginalPos] with HasExpenditure {
+    override def compareTo(that: ExpenditureByOriginalPos): Int = {
+      exp.originalPosition - that.exp.originalPosition
+    }
+  }
+
 
   def notificationCount(d: Int, expenditures: Seq[Int]) = {
-    /*
-    var window = mutable.PriorityQueue[(Int,Int)]()(Ordering.by[(Int,Int), Int](e => e._1).reverse)
-    (0 until d).map(i => (expenditures(i), i)).foreach(window += _)
-    */
-    //var window = (0 until d).map(i => (expenditures(i), i)).sortBy(x => x)
-    val sorted = expenditures.indices.map(x => (expenditures(x), x)).sortBy(_._1)
-    val dayAndSearchIndices = (d until sorted.size)
-      .map(sorted(_)._2)
-      .map(day => {
-        val middle = day - Math.ceil(d / 2.0).toInt
-        val medianIndicesSearchEnd =
-          if (d % 2 == 0 && d > 1) {
-            Seq(middle, middle - 1)
-          } else Seq(middle)
-        (day, medianIndicesSearchEnd)
-      })
-    val dayAndMedians = dayAndSearchIndices.map(dayAndSearchEnd => {
-      val medians =
-        dayAndSearchEnd._2.flatMap(searchEnd => (searchEnd to Math.max(dayAndSearchEnd._1 - d, 0) by -1)
-          .find(idx => {
-            val originalPos = sorted(idx)._2
-            originalPos >= dayAndSearchEnd._1 - d && originalPos < dayAndSearchEnd._1
-          }))
-        .map(x => sorted(x)._1)
-      (dayAndSearchEnd._1, medians)
-    })
-    val dayAndMedian = dayAndMedians
-      .map(dayAndMedians =>
-        if(dayAndMedians._2.size == 1)
-        (dayAndMedians._1, dayAndMedians._2.head.toDouble)
-        else
-          (dayAndMedians._1, dayAndMedians._2.sum.toDouble / 2))
-  val notificationCount = dayAndMedian
-      .map(dAndM => {
-        val todaysExpenditure = expenditures(dAndM._1)
-        if (todaysExpenditure >= dAndM._2.*(2.0)) 1 else 0
-      }).sum
+    val exps = expenditures.indices.map(i => Expenditure(expenditures(i), i))
+    val byAmount = new java.util.TreeSet[ExpenditureByAmount]()
+    exps.foreach(e => byAmount.add(ExpenditureByAmount(e)))
+    val byPos = new java.util.TreeSet[ExpenditureByOriginalPos]()
+    exps.foreach(e => byPos.add(ExpenditureByOriginalPos(e)))
+    1
+  }
 
+    /*
+    val notificationCount =
+    (d until expenditures.size)
+      .map(day => {
+        val todaysExpenditure = expenditures(day)
+        val medianExp = daysToMedians(day)
+        if (todaysExpenditure >= 2.0 * medianExp)
+          1
+        else
+          0
+      }).sum
     notificationCount
+  }
+  */
+
+  def medianElements(range: Seq[(Int,Int)]) = {
+    val middleIndex = range.size / 2
+    if (range.size % 2 == 0) {
+      range.slice(middleIndex - 1, middleIndex)
+    } else {
+      Seq(range(middleIndex))
+    }
   }
 
   def notificationsOnDay(recent: Seq[(Int,Int)], todaysExpenditure: Int) = {
